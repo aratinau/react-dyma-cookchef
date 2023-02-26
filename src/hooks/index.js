0ba166ba24d51a00790ab215afa5238586f0c4cc
Aymeric Ratinaud <aymeric.ratinaud@sfr.fr>
2023-02-26 13:04:10 +0100

    form validation and fetch query

---
+++ src/hooks/index.js
@@ -0,0 +1,7 @@
+export * from './useFetchData';
+
+/*
+* Un index est une bonne pratique pour faciliter l'import d'objets dans votre application.
+* Ici si vous aviez de nombreuses hooks personnalisés vous pourriez les importer directement
+* depuis le dossier hooks sans avoir à spécifier le chemin entier.
+*/
